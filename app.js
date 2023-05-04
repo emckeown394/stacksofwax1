15566f4e1082f75ad3d0224456d57b2cafc4fb3f
emckeown394 <125586249+emckeown394@users.noreply.github.com>
2023-05-04 18:29:24 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+const express = require("express");
+let app = express();
+const path = require("path");
+const connection = require("./connection.js");
+
+//middleware
+app.use(express.static(path.join(__dirname,'./public')));
+app.set('view engine', 'ejs');
+
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
@@ -5,3 +14,7 @@ menu.addEventListener('click', function() {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
 });
+
+//server
+app.listen(process.env.PORT || 3000);
+console.log(" Server is listening on //localhost:3000/ ");
